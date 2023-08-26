d49b160ca5e1cc332865da18f2888043962b9a1a
TalelMejri <talelmejri8@gmail.com>
2023-08-26 13:51:24 +0100

    initProject Sctp

---
+++ backend/sctp/src/main/java/app/sctp/SctpApplication.java
@@ -0,0 +1,13 @@
+package app.sctp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SctpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SctpApplication.class, args);
+	}
+
+}
---
+++ backend/sctp/src/test/java/app/sctp/SctpApplicationTests.java
@@ -0,0 +1,13 @@
+package app.sctp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SctpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
