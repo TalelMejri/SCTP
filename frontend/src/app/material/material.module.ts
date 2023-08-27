64b72398b7cded228cca38847c18328854469fb2
TalelMejri <talelmejri8@gmail.com>
2023-08-27 17:15:53 +0100

    change logo

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -5,21 +5,30 @@ import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from "@angular/material/icon"
 import {MatButtonModule }from "@angular/material/button"
 import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from "@angular/material/input"
+import {MatBadgeModule} from '@angular/material/badge';
 @NgModule({
   imports: [
     CommonModule,
     MatSelectModule,
+    MatDividerModule,
     MatSlideToggleModule,
     MatButtonModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatBadgeModule
   ],
   exports:[
     MatSlideToggleModule,
     MatCardModule,
     MatIconModule,
     MatButtonModule,
-    MatSelectModule
+    MatDividerModule,
+    MatSelectModule,
+    MatInputModule,
+    MatBadgeModule
   ]
 })
 export class MaterialModule { }
