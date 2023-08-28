cb902be763a9ba259e44977c3af4e8ab2ef8e8cb
TalelMejri <talelmejri8@gmail.com>
2023-08-28 15:22:06 +0100

    add footer component

---
+++ frontend/src/app/Components/foooter-compo/foooter-compo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foooter-compo',
+  templateUrl: './foooter-compo.component.html',
+  styleUrls: ['./foooter-compo.component.scss']
+})
+export class FoooterCompoComponent {
+
+}
