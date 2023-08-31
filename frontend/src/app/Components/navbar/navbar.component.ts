a2d03894ab27626c6f7d0a96a55d00792738610d
TalelMejri <talelmejri8@gmail.com>
2023-08-31 10:47:17 +0100

    test authentication

--- frontend/src/app/Components/navbar/navbar.component.ts
+++ frontend/src/app/Components/navbar/navbar.component.ts
@@ -1,20 +1,36 @@
-import { Component,Input,Output,EventEmitter, HostListener} from '@angular/core';
+import { Component,Input,Output,EventEmitter, OnInit} from '@angular/core';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { Router } from '@angular/router';
+import {Store} from "@ngxs/store"
+import { AuthServiceService } from 'src/app/Services/auth-service.service';
+import { Logout } from 'src/app/Store/state';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent  implements OnInit   {
 
+    User:any=[];
+    ngOnInit(): void {
+       this.User= this.Store.selectSnapshot((state)=>state.AuthStore?.User);
+    }
    isChecked=false;
    choseList:string="mat";
-
+    constructor(public AuthServiceService:AuthServiceService,private Store:Store,private router:Router){}
  
   
    @Input() modeCurrent:string="nightlight_round";
    @Output() changeMode=new EventEmitter<MatSlideToggleChange>();
-
-
+   
+   
+   LogoutUser(){
+    this.Store.dispatch([
+      new Logout()
+    ]);
+    localStorage.removeItem("token");
+    this.router.navigate([''])
+    location.reload();//refresh
+   }
 }
