ebcd412fe95fe1fac20530f5245ec7199d96a7be
TalelMejri <talelmejri8@gmail.com>
2023-08-31 10:47:36 +0100

    add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponentComponent } from './Components/home-component/home-component.component';
 import { SignUpCompComponent } from './Components/Auth/sign-up-comp/sign-up-comp.component';
+import { LoginCompComponent } from './Components/Auth/login-comp/login-comp.component';
+import { DashboardComponent } from './AdminSide/dashboard/dashboard.component';
 
 
 const routes: Routes = [
   {path:"",component:HomeComponentComponent},
-  {path:"signup",component:SignUpCompComponent}
+  {path:"signup",component:SignUpCompComponent},
+  {path:"login",component:LoginCompComponent},
+  {path:"dashboard",component:DashboardComponent}
 ];
 
 @NgModule({
