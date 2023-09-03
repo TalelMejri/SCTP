6ee7a3020dc77f3749645b5387a82661ee5b7067
TalelMejri <talelmejri8@gmail.com>
2023-09-03 11:13:27 +0100

    designDahabdoard

--- frontend/src/app/AdminSide/dashboard/dashboard.component.ts
+++ frontend/src/app/AdminSide/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-
+  isSidebarOpen = false;
+  
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+    
+ 
 }
